3115dfbb5362160f57553aaefbbfe94a6bb9d87f
juliwhite <julietawhite30@gmail.com>
2024-09-17 10:54:13 -0600

    add assignment lesson 1

---
+++ controllers/lesson1.js
@@ -0,0 +1,5 @@
+const juliRoute = (req, res) => {
+    res.send("Julieta White");
+  };
+
+module.exports = { juliRoute };
---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const lesson1Controller = require('./controllers/lesson1');
+ 
+app.get('/', lesson1Controller.juliRoute);
+
+const port = 3000;
+ 
+app.listen(process.env.PORT || port, () => {
+  console.log('Web Server is listening at port ' + (process.env.PORT || 3000));
+});
