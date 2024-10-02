87ad9baaf44c2fba9ff7b4a85545b49bbb264a22
juliwhite <julietawhite30@gmail.com>
2024-10-02 10:07:22 -0600

    lesson3 added

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator');
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId; // Ensure you import ObjectId
 
@@ -23,5 +24,77 @@ const getAll = async (req, res, next) => {
     });
   };
 
+// CREATE a New Contact (lesson3)
+const createContact = async (req, res, next) => {
+  try{
+    const newContact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+      };
 
-  module.exports = { getAll, getSingle };
+    const result = await mongodb.getDb().collection('contacts').insertOne(newContact);
+
+    res.setHeader('Content-Type', 'application/json');
+    res.status(201).json({message: "Contact created successfully.", contactId: result.insertedId});
+
+  } catch (err) {
+    res.status(500).json({ message: 'Failded to create contact.', error: err});
+
+  }
+}
+
+// UPDATE CONTACT
+const updateContact = async (req, res) => {
+  //Validate request
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const contactId = new ObjectId(req.params.id); // Get the contact ID from URL
+  const updateContact = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
+  };
+
+  try {
+    const result = await mongodb.getDb().collection('contacts').updateOne(
+      { _id: contactId },  // Find the docoment by its _id
+      { $set: updateContact } //Update the document
+    );
+
+    if (result.matchedCount === 0) {
+      res.status(404).json({ message: 'Contact not found' });
+    } else {
+      res.status(200).json({ message: 'Contact updated successfully' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while udpating the contact' });
+  }
+};
+
+// DELETE CONTACT BY ID
+const deleteContact = async (req, res) => {
+  const contactId = new ObjectId(req.params.id);  // Get the contact ID from URL
+
+  try {
+    const result = await mongodb.getDb().collection('contacts').deleteOne({ _id: contactId });
+
+    if (result.deleteCount === 0) {
+      res.status(404).json({ message: 'Contact not found' });
+    } else {
+      res.status(200).json({ message: 'Contact deleted successfully' });
+    }
+  } catch (error) {
+    res.status(500).json({ error: 'An error occurred while deleting the contact '});
+  }
+};
+
+
+module.exports = { getAll, getSingle, createContact, updateContact, deleteContact };
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');  // Import the validation functions
 
 const contactsController = require('../controllers/contacts');
 
 router.get('/', contactsController.getAll);
 
 router.get('/:id', contactsController.getSingle);
+
+// Route to create a new contact (POST)
+router.post('/', [
+    check('firstName').not().isEmpty().withMessage('First name is required'),
+    check('lastName').not().isEmpty().withMessage('Last name is required'),
+    check('email').isEmail().withMessage('Valid email is required.'),
+    check('favoriteColor').not().isEmpty().withMessage('Favorite color is required.'),
+    check('birthday').not().isEmpty().withMessage('Valid birthday is required.')
+], (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+}, contactsController.createContact);
+
+// Route to update a contact.
+router.put('/:id', [
+    check('firstName').not().isEmpty().withMessage('First name is required'),
+    check('lastName').not().isEmpty().withMessage('Last name is required'),
+    check('email').isEmail().withMessage('Valid email is required'),
+    check('favoriteColor').not().isEmpty().withMessage('Favorite color is required'),
+    check('birthday').not().isEmpty().withMessage('Birthday is required'),
+], contactsController.updateContact);
+
+// Route to DELETE a contact by ID
+router.delete('/:id', contactsController.deleteContact);
 
 module.exports = router;
